2228eadc724af344933a2ba2ffc733cadf27db9b
Yashwanth <yashwanth.hs@siemens.com>
2021-05-23 22:43:42 +0530

    feat: contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -27,7 +30,11 @@ import { ContactComponent } from './contact/contact.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import "../../assets/smtp.js";
+declare let Email: any;
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('')
+  });
 
-  ngOnInit(): void {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  sendMail(msgForm: any){
+    const userName: string = msgForm?.value?.name;
+    const userMailId: string = msgForm?.value?.email;
+    const message:string = msgForm?.value?.message;
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "yashwanthhs07@gmail.com",
+      Password : "********************",
+      To : "yashwanthhs07@gmail.com",
+      From : userMailId,
+      Subject : "Test email",
+      Body : `Hi ${userName}, Received your message: ${message} Thank you!`
+    }).then((message: any) => alert(message));
   }
-
 }
